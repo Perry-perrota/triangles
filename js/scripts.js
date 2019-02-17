db992e7a033b18716abb7cf7addbc2030f37b077
Perry-perrota <perryobara@gmail.com>
2019-02-18 00:10:54 +0300

    add check for isosceles triangle

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,19 @@ function tcheck(){
   var c=parseInt(document.getElementById("num3").value);
   var dimensions=[];
   dimensions.push(a,b,c);
-if (dimensions[0] + dimensions[1] > dimensions[2] && dimensions[1] + dimensions[2] > dimensions[0] && dimensions[0] + dimensions[2] > dimensions[1]===true ) {
-  //check for equilateral triangle
-  if (dimensions[0]===dimensions[1] && dimensions[1]===dimensions[2]===true) {
-    alert("this is an equilateral");
+if(dimensions[0] + dimensions[1] > dimensions[2] && dimensions[1] + dimensions[2] > dimensions[0] && dimensions[0] + dimensions[2] > dimensions[1]===true) {
+  if (dimensions[0]==dimensions[1] && dimensions[1]==dimensions[2]){
+    alert("This is an equilateral triangle")
   }
   //check for isosceles triangle
-  else if (dimensions[0]===dimensions[1]) && dimensions[1]!==dimensions[2]===true {
-    alert("this is an isosceles");
+  else if (dimensions[0]==dimensions[1] && dimensions[1]!==dimensions[2]) {
+    alert("This is an isosceles triangle")
   }
-  else (dimensions[0]!==dimensions[1]) && dimensions[1]!==dimensions[2]===true {
-    alert("this is an scalene");
+  else {
+    alert("This is a scalene triangle")
   }
-}
+  }
 else {
     alert("not able to form a triangle.Try again with other values");
 }
-
-
-
 }
