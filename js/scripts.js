cc6631661922249c60a934c47d000856eb26584e
Perry-perrota <perryobara@gmail.com>
2019-02-17 21:55:37 +0300

    adds the input values to the javascript function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,7 @@
-function tcheck(a,b,c){
+function tcheck(){
+  var a=parseInt(document.getElementById("num1").value);
+  var b=parseInt(document.getElementById("num2").value);
+  var c=parseInt(document.getElementById("num3").value);
 
-var a = parseInt(document.getElementById("num1"));
-var b = parseInt(document.getElementById("num2"));
-var c = parseInt(document.getElementById("num3"));
 
-//check if its a Triangle
-if (a+b>c || b+c>a || a+c>b){
-//check for equilateral Triangle
-if (a===b===c){
-  alert("This is an equilateral triangle");
- }
- // check for an isosceles triangle
-  else if(a===b || b===c || a===c){
-    alert("this is an isosceles triangle");
-  }
-//check for a scalene triangle
-  else
-{
-  alert("is a scalene Triangle")
-}
-}
-else {
-  alert("Its not a triangle");
-}
 }
